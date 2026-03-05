b37eb3f781e0d0ec17a7642a5e4669212fe2ecf0
Carlos Eduardo <145712990+ic4rlos@users.noreply.github.com>
2026-03-05 17:11:58 -0300

    Update c-new-password.tsx

--- pages/c-new-password.tsx
+++ pages/c-new-password.tsx
@@ -76,7 +76,7 @@ export default function CNewPassword() {
       >
         {/* LOGO */}
         <img
-          src="/plasmic/ez_marketing_platform/images/logo2Svg.svg"
+          src="/plasmic/ez_marketing_platform_sacanagem/images/logo2Svg.svg"
           alt="ezmarketing"
           style={{
             width: 700,

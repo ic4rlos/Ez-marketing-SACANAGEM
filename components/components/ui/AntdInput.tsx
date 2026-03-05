8e222e6762d92ff14245187a238963f95ccf7bf6
Carlos Eduardo <145712990+ic4rlos@users.noreply.github.com>
2026-03-05 16:44:43 -0300

    Create AntdInput.tsx

---
+++ components/components/ui/AntdInput.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import * as React from "react";
+import { AntdInput } from "@plasmicpkgs/antd5/skinny/registerInput";
+
+type Props = {
+  type?: string;
+  value?: string;
+  placeholder?: string;
+  className?: string;
+  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+};
+
+export default function Input(props: Props) {
+  return <AntdInput {...props} />;
+}

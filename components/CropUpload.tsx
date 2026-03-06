ac2c36df5f40d6c7b9bc0e268eeace186f89972d
Carlos Eduardo <145712990+ic4rlos@users.noreply.github.com>
2026-03-05 21:21:42 -0300

    Update CropUpload.tsx

--- components/CropUpload.tsx
+++ components/CropUpload.tsx
@@ -5,8 +5,8 @@ import ImgCrop from "antd-img-crop";
 import { Upload } from "antd";
 import type { UploadProps, UploadFile } from "antd";
 import { PlusOutlined } from "@ant-design/icons";
-import { supabaseCompany } from "../lib/company-supabaseClient";
-import { supabaseAgency } from "../lib/agency-supabaseClient";
+import { getSupabaseC } from "../lib/company-supabaseClient";
+import { getSupabaseA } from "../lib/agency-supabaseClient";
 
 export interface CropUploadProps {
   className?: string;

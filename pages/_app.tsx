f9cfa94ed196ff27b0462a8a5ec1d3d398955967
Carlos Eduardo <145712990+ic4rlos@users.noreply.github.com>
2026-03-12 16:47:49 -0300

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,7 @@
 import type { AppProps } from "next/app";
 import { PlasmicRootProvider } from "@plasmicapp/react-web";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
